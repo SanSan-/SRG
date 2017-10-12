bc843b6caa9bb3db79da04eb6652e3de87143cf2
alexfranz <ut1339@gmail.com>
2017-10-12 16:38:53 +0300

    fix-route: - set false route in Login

--- frontend/player/src/views/Login.vue
+++ frontend/player/src/views/Login.vue
@@ -67,6 +67,14 @@
         error: ''
       }
     },
+    computed: {
+      ...mapGetters([
+        'currentUser'
+      ]),
+      isAuthenticated() {
+        return this.currentUser.email !== null;
+      }
+    },
     methods: {
       submit() {
         let credentials = {
@@ -76,7 +84,23 @@
         // We need to pass the component's this context
         // to properly make use of http in the auth service
         this.$store.dispatch('login', this, credentials, 'MyGames')
+      },
+      checkAuth() {
+        if (this.isAuthenticated) {
+          router.push({
+              name: 'MyGames'
+            }
+          )
+        } else {
+          router.push({
+              name: 'Login'
+            }
+          )
+        }
       }
+    },
+    created() {
+      this.checkAuth()
     }
   }
 </script>
